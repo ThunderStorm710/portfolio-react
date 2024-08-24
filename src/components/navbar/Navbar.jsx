6892f83bb0c60977a98ecec48af289f7a136840b
Pedro Ascensão <pascensao77@gmail.com>
2024-08-24 01:32:07 +0100

    Portfolio + About + Home + Contact

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon, faBars } from '@fortawesome/free-solid-svg-icons';
@@ -11,25 +11,30 @@ function Navbar({ theme, toggleTheme }) {
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
         <nav className={`navbar ${theme}`}>
             <div className="logo">Pedro Ascensão</div>
             <ul className={`nav-links ${menuOpen ? 'mobile-nav' : ''}`}>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/" onClick={closeMenu}>Home</Link>
                 </li>
                 <li>
-                    <Link to="/about">About</Link>
+                    <Link to="/about" onClick={closeMenu}>About</Link>
                 </li>
                 <li>
-                    <Link to="/portfolio">Portfolio</Link>
+                    <Link to="/portfolio" onClick={closeMenu}>Portfolio</Link>
                 </li>
                 <li>
-                    <Link to="/contact">Contact</Link>
+                    <Link to="/contact" onClick={closeMenu}>Contact</Link>
                 </li>
             </ul>
             <button className="theme-toggle" onClick={toggleTheme}>
-                <FontAwesomeIcon icon={theme === 'light' ? faSun : faMoon}/>
+                <FontAwesomeIcon icon={theme === 'light' ? faSun : faMoon} />
             </button>
             <button className="mobile-menu-toggle" onClick={toggleMenu}>
                 <FontAwesomeIcon icon={faBars} />
